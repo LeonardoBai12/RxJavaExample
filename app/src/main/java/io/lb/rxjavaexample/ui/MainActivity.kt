40ab16c681806ff697f81ead92607efd0da79011
LeonardoBai12 <69223422+LeonardoBai12@users.noreply.github.com>
2022-02-01 10:39:35 -0300

    Alterado gradle para que seja possível utilizar viewBindings e adicionados packages padrão

---
+++ app/src/main/java/io/lb/rxjavaexample/ui/MainActivity.kt
@@ -0,0 +1,17 @@
+package io.lb.rxjavaexample.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.lb.rxjavaexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
